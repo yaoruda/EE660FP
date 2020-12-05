420b61a26dff7ba826b309da7927fe0dea92b16b
Ruda Yao <rudayao@usc.edu>
2020-12-06 06:09:10 +0800

    1

--- main.py
+++ main.py
@@ -41,12 +41,12 @@ if model=='SVC':
     result = {}
     n_component = [0.5, 0.9]
     for n_c in n_component:
-        X_train, features = preprocess_cls.PCA(X_train, features, n_c)
-        X_val, features = preprocess_cls.PCA(X_val, features, n_c)
-        X_test, features = preprocess_cls.PCA(X_test, features, n_c)
+        X_train, new_features = preprocess_cls.PCA(X_train, features, n_c)
+        X_val, new_features = preprocess_cls.PCA(X_val, features, n_c)
+        X_test, new_features = preprocess_cls.PCA(X_test, features, n_c)
 
         for k in kernels:
-            svc = SVC(X_train, X_val, X_test, y_train, y_val, y_test, features, targets_scored_col_name)
+            svc = SVC(X_train, X_val, X_test, y_train, y_val, y_test, new_features, targets_scored_col_name)
             svc.SVC_train(k)
             r = svc.SVC_validation()
             result[f'{k,n_c}'] = r
@@ -57,12 +57,12 @@ if model=='SVC':
 # if model=='1-3':
 # (1.3) SVC with statistic features
     # preprocessing
-    X_train, features = preprocess_cls.feature_statistic(X_train)
-    X_val, features = preprocess_cls.feature_statistic(X_val)
-    X_test, features = preprocess_cls.feature_statistic(X_test)
+    X_train, new_features = preprocess_cls.feature_statistic(X_train)
+    X_val, new_features = preprocess_cls.feature_statistic(X_val)
+    X_test, new_features = preprocess_cls.feature_statistic(X_test)
     result = {}
     for k in kernels:
-        svc = SVC(X_train, X_val, X_test, y_train, y_val, y_test, features, targets_scored_col_name)
+        svc = SVC(X_train, X_val, X_test, y_train, y_val, y_test, new_features, targets_scored_col_name)
         svc.SVC_train(k)
         r = svc.SVC_validation()
         result[k] = r
